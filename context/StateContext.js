39a298a2a44c6fb660f94a490a9dd2af7d4dee2a
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-08-17 18:27:28 +0700

    Done All Success Page Begin To Deployment

--- context/StateContext.js
+++ context/StateContext.js
@@ -100,6 +100,9 @@ export const StateContext = ({ children }) => {
         setShowCart,
         toggleCartItemQuantity,
         onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
       }}
     >
       {children}
